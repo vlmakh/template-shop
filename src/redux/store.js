9f6e5e328951bc07675b46e24b19f4f5fc5692de
Vlad Makhnenko <vlmakh@gmail.com>
2023-03-04 13:07:54 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -18,7 +18,7 @@ import { cartSlice } from './cart';
 const authPersistConfig = {
   key: 'auth',
   storage,
-  whitelist: ['token'],
+  // whitelist: ['token'],
 };
 
 // const cartPersistConfig = {
