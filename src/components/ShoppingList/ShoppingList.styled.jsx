f7746cdbdb18f2f813a0baabc38150db2345b949
Vlad Makhnenko <vlmakh@gmail.com>
2023-03-04 09:15:36 +0200

    update ShoppingCart

---
+++ src/components/ShoppingList/ShoppingList.styled.jsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const ShopTable = styled.table`
+  max-width: 640px;
+  margin: 16px auto 0;
+  background-color: white;
+
+  th {
+    padding: 8px 0;
+    text-transform: uppercase;
+    background-color: darkcyan;
+    color: #fff;
+  }
+
+  td {    
+    padding: 8px 0;
+    font-size: 12px;
+    font-weight: 500;
+    border: 1px solid darkgrey;
+  }
+
+  td:first-of-type {
+    width: 80%;
+    text-align: left;
+    padding-left: 8px;
+    padding-right: 8px;
+  }
+
+  tr:nth-of-type(2n + 1) {
+    background-color: lightblue;
+  }
+`;
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,9 +1,10 @@
 import { Box } from 'components/Box/Box';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn, selectCart } from 'redux/selectors';
 import { fetchSelected } from 'redux/operations';
 import { useEffect, useState } from 'react';
+import { ShopTable } from 'components/ShoppingList/ShoppingList.styled';
 
 export const ShoppingCart = () => {
   const selected = useSelector(selectCart);
@@ -13,9 +14,15 @@ export const ShoppingCart = () => {
   useEffect(() => {
     fetchSelected(selected).then(data => {
       setCart(data);
-      console.log(data);
+      // console.log(data);
     });
   }, [selected]);
+
+  const totalSum = arr => {
+    return arr.reduce((acc, el) => {
+      return acc + Number(el.price);
+    }, 0);
+  };
 
   return (
     <>
@@ -23,12 +30,38 @@ export const ShoppingCart = () => {
 
       <Box textAlign="center" mt={6}>
         <h2>SHOPPING CART</h2>
+
+        {!cart.length && <p>Your Shopping cart is empty</p>}
+
         {cart.length > 0 && (
-          <ul>
-            {cart.map(product => {
-              return <li key={product.id}>name: {product.title}</li>;
-            })}
-          </ul>
+          <ShopTable>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {cart.map((item, idx) => {
+                return (
+                  <tr key={idx}>
+                    <td>
+                      <Link to={`/products/${item.id}`}>{item.title} </Link>
+                    </td>
+                    <td>{item.price.toFixed(2)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+
+            <tfoot>
+              <tr>
+                <td>Total USD: </td>
+                <td>{totalSum(cart)}</td>
+              </tr>
+            </tfoot>
+          </ShopTable>
         )}
       </Box>
     </>
