99fb5688751984660d1274c307e9fc01012c403d
Vlad Makhnenko <vlmakh@gmail.com>
2023-03-06 10:34:47 +0200

    Update RegisterPage.js

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -2,16 +2,16 @@ import { Box } from 'components/Box/Box';
 import { LoginBox } from 'components/LoginBox/LoginBox';
 import { MenuLink } from 'components/Login/Login.styled';
 import { Register } from 'components/Register/Register';
-// import { useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
-// import { selectIsLoogedIn } from 'redux/selectors';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectIsLoggedIn } from 'redux/selectors';
 
 export const RegisterPage = () => {
-  //   const isLoggedIn = useSelector(selectIsLoogedIn);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
 
   return (
     <Box>
-      {/* {isLoggedIn && <Navigate to="/contacts" />} */}
+      {isLoggedIn && <Navigate to="/cabinet" />}
 
       <LoginBox>
         <Box display="flex" justifyContent="space-around">
