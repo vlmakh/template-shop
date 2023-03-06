07b43a2af4698bb4899ab08c1beb1b79829d0a9e
Vlad Makhnenko <106901780+vlmakh@users.noreply.github.com>
2023-03-06 07:57:58 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/react-template-mpa">
+        <BrowserRouter basename="/template-shop">
           <App />
         </BrowserRouter>
       </PersistGate>
